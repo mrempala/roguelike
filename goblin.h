122f834d83344a0a4f1fed6fc2985f061be78f99
mrempala <mrempala@users.noreply.github.com>
2014-10-24 16:41:55 -0700

    Added design.txt

--- goblin.h
+++ goblin.h
@@ -5,12 +5,14 @@ class game;
 
 class goblin {
    public:
+      bool validMove(int x, int y, const game* mainGame);
+      void moveRandomly(const game *mainGame);
+      void spawn(const game* mainGame); //Not implemented
+      
       void setX(int a){x = a;}
       void setY(int a){y = a;}
       int getX()const{return x;}
       int getY()const{return y;}
-      bool validMove(int x, int y, const game* mainGame);
-      void moveRandomly(const game *mainGame);
       
    private:
       int health; //not used
